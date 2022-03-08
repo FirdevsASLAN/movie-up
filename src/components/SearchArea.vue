073b858daf0ddde68d670c8145f24db6ee3b6b76
Firdevs Aslan <firdevsaslan6@gmail.com>
2022-03-08 16:03:17 +0300

    create SearchArea components.

---
+++ src/components/SearchArea.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="white">
+    <v-container class="search__container d-flex">
+      <v-select :items="items" label="Year" outlined></v-select>
+      <v-select :items="items" label="Type" outlined></v-select>
+      <v-text-field
+        outlined
+        label="Enter movie name here"
+        append-icon="mdi-magnify"
+      ></v-text-field>
+      <v-btn elevation="2" color="amber accent-3" dark>
+        <span> Search </span>
+        <v-icon>mdi-arrow-right</v-icon>
+      </v-btn>
+    </v-container>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.search {
+  &__container {
+    max-width: 75rem;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <home-header />
+    <search-area />
   </div>
 </template>
 
 <script>
 import HomeHeader from "@/components/HomeHeader.vue";
+import SearchArea from "@/components/SearchArea.vue";
 
 export default {
   name: "HomeView",
   components: {
     HomeHeader,
+    SearchArea,
   },
 };
 </script>
