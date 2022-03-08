ee51b1c83b74b02b17a363dc07c5ef8426473d0b
Firdevs Aslan <firdevsaslan6@gmail.com>
2022-03-08 15:30:56 +0300

    add header title and header paragraph.

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,8 +1,15 @@
 <template>
   <section class="header">
     <v-container>
-      <h1>Header</h1>
-    </v-container>
+      <h1 class="header__title text-h2 font-weight-medium white--text">
+        Welcome to <b>MovieUP.</b>
+      </h1>
+      <p class="header__paragraph white--text mt-8">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris
+      </p></v-container
+    >
   </section>
 </template>
 
@@ -16,5 +23,12 @@ export default {
 .header {
   aspect-ratio: 2.78;
   background-image: url("@/assets/header-background.png");
+  padding-top: 7.9rem;
+  &__paragraph {
+    max-width: 36.5rem;
+  }
+  &__title {
+    max-width: 34rem;
+  }
 }
 </style>
